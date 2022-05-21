4449a895965d6638738b64e585333ac495632282
zhangguangyong <1243610991@qq.com>
2022-05-21 12:43:15 +0800

    布局01

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,22 @@
 import {FC, ReactElement} from 'react'
 import './index.scss'
 import {Main} from 'components/Layout/Main'
+import {Nav} from 'components/Layout/Nav'
 
 export const Layout: FC = (): ReactElement => {
   return (
     <div className="layout-component">
-      <div className="main">
-        <Main/>
+      <div className={'top'}>
+        top
+      </div>
+
+      <div className={'bottom'}>
+        <div className={'left'}>
+          <Nav/>
+        </div>
+        <div className={'right'}>
+          <Main/>
+        </div>
       </div>
     </div>
   )
