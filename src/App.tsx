c0f262ea8ab530f1717a9dc6f4da06dbe8357f86
zhangguangyong <1243610991@qq.com>
2022-05-21 10:44:33 +0800

    基础框架01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react'
+import './App.css'
+import {Layout} from 'components/Layout'
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 
-function App() {
-  return (
+
+const App: FC = () => (
+  <BrowserRouter basename={'/games'}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Navigate to="/snake"/>}/>
+        </Route>
+      </Routes>
     </div>
-  );
-}
+  </BrowserRouter>
+)
 
-export default App;
+export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import {FC, ReactElement} from 'react'
+import './index.scss'
+
+export const Home: FC = (): ReactElement => {
+  return (
+    <div className={'home-page'}>
+      首页
+    </div>
+  )
+}
