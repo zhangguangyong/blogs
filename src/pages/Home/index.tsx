50ed93467a2b55bc3e1635c12ac4ff75ae382b7b
zhangguangyong <1243610991@qq.com>
2022-05-22 11:15:04 +0800

    代码编辑器01

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,64 @@
-import {FC, ReactElement} from 'react'
+import {FC, ReactElement, useState} from 'react'
 import './index.scss'
 
-import CodeMirror from '@uiw/react-codemirror'
-import {javascript} from '@codemirror/lang-javascript'
+import CodeMirror, {ViewUpdate} from '@uiw/react-codemirror'
+import {markdown, markdownLanguage} from '@codemirror/lang-markdown'
+import {languages} from '@codemirror/language-data'
+import {marked} from 'marked'
+import highlight from 'highlight.js'
+import 'highlight.js/styles/github.css'
 
 export const Home: FC = (): ReactElement => {
+  const code = `## Title
+\`\`\`jsx
+function Demo() {
+  return <div>demo</div>
+}
+\`\`\`
+
+\`\`\`bash
+# Not dependent on uiw.
+npm install @codemirror/lang-markdown --save
+npm install @codemirror/language-data --save
+\`\`\`
+
+[weisit ulr](https://uiwjs.github.io/react-codemirror/)
+
+\`\`\`go
+package main
+import "fmt"
+func main() {
+  fmt.Println("Hello, 世界")
+}
+\`\`\`
+`
+  const markedOptions: marked.MarkedOptions = {
+    highlight(code: string, lang: string, callback?: (error: any, code?: string) => void): string | void {
+      return highlight.highlightAuto(code).value
+    }
+  }
+
+  const toMarked = (src: string) => {
+    return marked(src, markedOptions)
+  }
+
+  let [preview, setPreview] = useState(toMarked(code))
+  // code preview
+  const handleChange = (value: string, viewUpdate: ViewUpdate) => {
+    setPreview(toMarked(value))
+  }
+
   return (
     <div className={'home-page'}>
-      <CodeMirror
-        value="console.log('hello world!');"
-        height="100%"
-        extensions={[javascript({ jsx: true })]}
-        onChange={(value, viewUpdate) => {
-          console.log('value:', value);
-        }}
-      />
+      <div className={'code'}>
+        <CodeMirror
+          value={code}
+          extensions={[markdown({base: markdownLanguage, codeLanguages: languages})]}
+          onChange={handleChange}
+        />
+      </div>
+      <div className={'preview'} dangerouslySetInnerHTML={{__html: preview}}>
+      </div>
     </div>
   )
 }
